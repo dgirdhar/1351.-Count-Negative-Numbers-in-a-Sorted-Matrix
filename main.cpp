bf497a59c7809044a7c75ce4ff9c0ad22f806dfc
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-09-14 21:34:19 -0400

    Update main.cpp
    Not iterating the data if we find out any negative number. Got better results this time.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
         int count = 0;
+        int size = grid.size();
         
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[i].size(); ++j) {
+        for (int i = 0; i < size; ++i) {
+            int rowSize = grid[i].size();
+            
+            for (int j = 0; j < rowSize; ++j) {
                 if (grid[i][j] < 0) {
-                    count++;
+                    count += rowSize - j;
+                    break;
                 }
             }
         }
